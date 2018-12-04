f01b7573a59a034256705df67c5253ffdd7ffbea
Martin Pastore <martinpablopastore@gmail.com>
2018-12-04 10:03:45 -0300

    form

--- module.js
+++ module.js
@@ -2,5 +2,6 @@ const burger = require('burgerjs');
     
 burger.module([
     [{name: 'header'},{type: 'component'},{selector: 'nem-header'}, {properties: []}],
+    [{name: 'news-form'},{type: 'component'},{selector: 'nem-news-form'}, {properties: []}],
     [{name: 'dashboard'},{type: 'page'},{order: '0'},{route: '/dashboard'}],
 ]);
