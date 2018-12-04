4ab6868b2d986df49d203fab647b65353d84d1b7
Martin Pastore <martinpablopastore@gmail.com>
2018-12-04 20:32:15 -0300

    remove get

--- server/endpoints.js
+++ server/endpoints.js
@@ -4,10 +4,4 @@ const client = redis.createClient(process.env.REDIS_URL);
 exports.saveNews = function(req, res) {
     const val = JSON.stringify(req.body);
     client.set('news', val, redis.print);
-    client.get('news', function (error, result) {
-        if (error) {
-            res.json(error);
-        }
-        res.json(result);
-    });
 };
