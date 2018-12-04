1dfb6e0602320d3a78555ea80a5c69bcbed97e56
Martin Pastore <martinpablopastore@gmail.com>
2018-12-04 20:25:56 -0300

    stringify

--- server/endpoints.js
+++ server/endpoints.js
@@ -2,15 +2,12 @@ const redis = require('redis');
 const client = redis.createClient(process.env.REDIS_URL);
 
 exports.saveNews = function(req, res) {
-    client.set('news', req.body, redis.print);
+    const val = JSON.stringify(req.body);
+    client.set('news', val, redis.print);
     client.get('news', function (error, result) {
         if (error) {
-            error = JSON.stringify(error);
-            error = JSON.parse(error);
-            res.json({error: error});
+            res.json(error);
         }
-        result = JSON.stringify(result);
-        result = JSON.parse(result);
         res.json(result);
     });
 };
