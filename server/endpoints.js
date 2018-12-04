8095417d4d3166d9191627a2956824a5a07f5058
Martin Pastore <martinpablopastore@gmail.com>
2018-12-04 20:42:20 -0300

    fix

--- server/endpoints.js
+++ server/endpoints.js
@@ -3,5 +3,5 @@ const client = redis.createClient(process.env.REDIS_URL);
 
 exports.saveNews = function(req, res) {
     const val = JSON.stringify(req.body);
-    client.set('news', val, redis.print);
+    client.rpush(['news', val]);
 };
