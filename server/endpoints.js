a1f84e4b75440546b5849fbaa577907a4cf3fbe8
Martin Pastore <martinpablopastore@gmail.com>
2018-12-04 20:23:20 -0300

    fix number 10000

--- server/endpoints.js
+++ server/endpoints.js
@@ -5,8 +5,12 @@ exports.saveNews = function(req, res) {
     client.set('news', req.body, redis.print);
     client.get('news', function (error, result) {
         if (error) {
+            error = JSON.stringify(error);
+            error = JSON.parse(error);
             res.json({error: error});
         }
-        res.json({result: result});
+        result = JSON.stringify(result);
+        result = JSON.parse(result);
+        res.json(result);
     });
 };
