ffed5638013172321964f4b288b8f37c826fc561
Martin Pastore <martinpablopastore@gmail.com>
2018-12-05 11:01:51 -0300

    added redis

--- server/endpoints.js
+++ server/endpoints.js
@@ -1,19 +1,22 @@
 require('dotenv').config(); // read .env files
-const { Client } = require('pg');
+const client = require('redis').createClient(process.env.REDIS_URL);
+const async = require('async');
 
 exports.saveNews = function(req, res) {
-    const client = new Client({
-        connectionString: process.env.DATABASE_URL,
-        ssl: true,
-    });
-
-    client.connect();
-
-    client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, result) => {
-        if (err) res.json(err);
-        for (let row of res.rows) {
-            res.json(JSON.stringify(row));
+    client.set(`key${Math.random(0, 1000)}`, 'bla');
+    client.keys('*', function (err, keys) {
+        if (err) return console.log(err);
+        if(keys){
+            async.map(keys, function(key, cb) {
+                client.get(key, function (error, value) {
+                    if (error) return cb(error);
+                    cb(null, value)
+                });
+            }, function (error, results) {
+                if (error) return console.log(error);
+                console.log(results);
+                res.json({data:results});
+            });
         }
-        client.end();
     });
 };
