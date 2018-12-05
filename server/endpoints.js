77bc5ed4fcce67cab771e75f1cc686d3bd126889
Martin Pastore <martinpablopastore@gmail.com>
2018-12-05 10:34:29 -0300

    fi

--- server/endpoints.js
+++ server/endpoints.js
@@ -1,3 +1,19 @@
+require('dotenv').config(); // read .env files
+const { Client } = require('pg');
 
 exports.saveNews = function(req, res) {
+    const client = new Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: true,
+    });
+
+    client.connect();
+
+    client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, result) => {
+        if (err) res.json(err);
+        for (let row of res.rows) {
+            res.json(JSON.stringify(row));
+        }
+        client.end();
+    });
 };
