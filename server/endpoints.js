c7f08bc7d6fdfe103cc9941d13b5200185b5f15c
Martin Pastore <martinpablopastore@gmail.com>
2018-12-06 10:14:35 -0300

    fix

--- server/endpoints.js
+++ server/endpoints.js
@@ -5,19 +5,8 @@ const rand = require("random-key");
 
 exports.saveNews = function(req, res) {
     let data = JSON.stringify(req.body);
-    client.set(rand.generate(), data);
-    client.keys('*', function (err, keys) {
-        if (err) return console.log(err);
-        if(keys){
-            async.map(keys, function(key, cb) {
-                client.get(key, function (error, value) {
-                    if (error) return cb(error);
-                    cb(null, value)
-                });
-            }, function (error, results) {
-                if (error) res.status(500).json({message: 'Algo falló. Volve a intentar!', class: 'failed'});
-                res.status(200).json({message: 'Subido con éxito!', class: 'success'});
-            });
-        }
+    client.set(rand.generate(), data, function(err, reply) {
+        if (err) res.status(500).json({message: 'Algo falló. Volve a intentar!', class: 'failed'});
+        res.status(200).json({message: 'Subido con éxito!', class: 'success'});
     });
 };
