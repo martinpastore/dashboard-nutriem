f69da8a374fd71d155c0f48bf64f7204c21b535c
Martin Pastore <martinpablopastore@gmail.com>
2018-12-07 08:42:37 -0300

    fix endpoints

--- server/endpoints.js
+++ server/endpoints.js
@@ -4,9 +4,12 @@ const async = require('async');
 const rand = require("random-key");
 
 exports.saveNews = function(req, res) {
-    let data = JSON.stringify(req.body);
-    client.set(rand.generate(), data, function(err, reply) {
+    const data = JSON.stringify(req.body);
+    client.incr('id', function(err, id) {
         if (err) res.status(500).json({message: 'Algo falló. Volve a intentar!', class: 'failed'});
-        res.status(200).json({message: 'Subido con éxito!', class: 'success'});
+        client.set(id, data, function(err, reply) {
+            if (err) res.status(500).json({message: 'Algo falló. Volve a intentar!', class: 'failed'});
+            res.status(200).json({message: 'Subido con éxito!', class: 'success'});
+        });
     });
 };
