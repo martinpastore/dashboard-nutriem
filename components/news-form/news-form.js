6b82bf72b277041a9ad2d5ca261245609ed89fea
Martin Pastore <martinpablopastore@gmail.com>
2019-01-14 10:05:29 -0300

    b text

--- components/news-form/news-form.js
+++ components/news-form/news-form.js
@@ -5,29 +5,44 @@ $('#news').submit(function (e){
     $('#message').text('');
 
     const form = e.currentTarget.id;
-    getBase64($('#inputImagen')[0].files[0])
-        .then((data) => {
-            const news = {
-                fecha: getDate(),
-                image: data,
-                titulo: $('#inputTitulo').val(),
-                contenido: replaceBreakLines()
-            };
+    if ($('#inputImagen')[0].files[0]) {
+        getBase64($('#inputImagen')[0].files[0])
+            .then((data) => {
+                const news = {
+                    fecha: getDate(),
+                    image: data,
+                    titulo: $('#inputTitulo').val(),
+                    contenido: replaceBreakLines()
+                };
 
-            httpRequest('POST', '/news', news)
-                .then(function(data) {
-                    data = JSON.parse(data);
-                    $('#message').addClass(data.class);
-                    $('#message').text(data.message);
-                    cleanFields();
-                })
-                .catch(function (error) {
-                    error = JSON.parse(error);
-                    $('#message').addClass(error.class);
-                    $('#message').text(error.message);
-                })
-        });
+                sendRequest(news);
+            });
+    } else {
+        const news = {
+            fecha: getDate(),
+            image: '',
+            titulo: $('#inputTitulo').val(),
+            contenido: replaceBreakLines()
+        };
+
+        sendRequest(news);
+    }
 });
+
+function sendRequest(news) {
+    httpRequest('POST', '/news', news)
+        .then(function(data) {
+            data = JSON.parse(data);
+            $('#message').addClass(data.class);
+            $('#message').text(data.message);
+            cleanFields();
+        })
+        .catch(function (error) {
+            error = JSON.parse(error);
+            $('#message').addClass(error.class);
+            $('#message').text(error.message);
+        })
+}
 
 function getBase64(file) {
     return new Promise((resolve, reject) => {
@@ -69,4 +84,17 @@ function cleanFields() {
     $('.custom-file-label').text('Imagen');
     $('#inputContenido').val('');
     $('#inputTitulo').val('');
+}
+
+function markSelectedText () {
+    var textarea = document.getElementById('inputContenido');
+ 
+    var len = textarea.value.length;
+    var start = textarea.selectionStart;
+    var end = textarea.selectionEnd;
+    var sel = textarea.value.substring(start, end);
+   
+    var replace = '<b>' + sel + '</b>';
+   
+    textarea.value =  textarea.value.substring(0,start) + replace + textarea.value.substring(end,len);
 }
